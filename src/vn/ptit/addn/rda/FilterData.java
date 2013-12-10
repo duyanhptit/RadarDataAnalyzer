e72152b3f517abcae87cb93fa7612f987118b62e
duyanhptit <duyanhptit@gmail.com>
2013-12-10 14:50:17 +0700

    lay xong cac frame chu du lieu gi vao file radarData.log

---
+++ src/vn/ptit/addn/rda/FilterData.java
@@ -0,0 +1,84 @@
+package vn.ptit.addn.rda;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterData {
+
+	private List<String> mPathFiles;
+	private OutputStream mOutput = null;
+
+	public FilterData(String pathInput) {
+		mPathFiles = getAllFileName(pathInput);
+		createOutput();
+	}
+
+	// get all file into E:\Radar data\data and build list file input path
+	private List<String> getAllFileName(String pathInput) {
+		List<String> pathFiles = new ArrayList<String>();
+		File folder = new File(pathInput);
+		for (File fileEntry : folder.listFiles()) {
+			if (!fileEntry.isDirectory()) {
+				pathFiles.add(pathInput + "/" + fileEntry.getName());
+			}
+		}
+		return pathFiles;
+	}
+
+	private void createOutput() {
+		try {
+			File file = new File(main.PATH_OUTPUT);
+			file.getParentFile().mkdirs();
+			mOutput = new FileOutputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void filterRawData() {
+		try {
+			for (String pathFile : mPathFiles) {
+				System.out.println("readding file " + pathFile);
+				InputStream in = new FileInputStream(pathFile);
+				byte[] buffer = new byte[1024];
+				while (in.read(buffer) > 1) {
+					int b = buffer[0] & 0xFF; // Note: b & 0xFF to Integer
+					if (b != 0xFF) {
+						processFrame(buffer);
+					}
+				}
+			}
+			mOutput.flush();
+			mOutput.close();
+			System.out.println("Writed data to " + main.PATH_OUTPUT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void processFrame(byte[] buffer) {
+		try {
+			mOutput.write(buffer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showPathFiles() {
+		for (String pathFile : mPathFiles) {
+			System.out.println(pathFile);
+		}
+	}
+
+	// byte b1 = buffer[0];
+	// String s1 = String.format("%8s", Integer.toBinaryString(b1 &
+	// 0xFF)).replace(" ", "0");
+	// System.out.println(s1);
+}
--- src/vn/ptit/addn/rda/main.java
+++ src/vn/ptit/addn/rda/main.java
@@ -2,10 +2,13 @@ package vn.ptit.addn.rda;
 
 public class main {
 
+	// private static final String PATH_INPUT = "E:/Radar data/data";
+	private static final String PATH_INPUT = "D:/Radar data/data";
+	public static final String PATH_OUTPUT = "data/radarData.log";
+
 	public static void main(String[] args) {
-		ReadData readData = new ReadData();
-		readData.showPathFiles();
-		readData.readAllRawData();
+		FilterData filterData = new FilterData(PATH_INPUT);
+		filterData.filterRawData();
 	}
 
 }
