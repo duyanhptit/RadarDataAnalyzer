ac3d181b0ca4e2241d64f021a9791c132ce251ed
duyanhptit <duyanhptit@gmail.com>
2013-12-12 01:21:30 +0700

    xong doc du lieu ra ma tran

--- src/vn/ptit/addn/rda/ProcessingData.java
+++ src/vn/ptit/addn/rda/ProcessingData.java
@@ -3,14 +3,17 @@ package vn.ptit.addn.rda;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.TreeMap;
 
 public class ProcessingData {
 
 	private String mPathData;
-	private int[][] mRadarImg = new int[0x3FFF + 1][3000];
-	private int mIndexRadarImg = 0;
-	private int[] mTempPulseData = new int[3000];
+	private TreeMap<Integer, int[][]> mImg = new TreeMap<Integer, int[][]>();
+	private int[][] mRadarImg = new int[20000][3000];
+	private int[] mTempPulseData = new int[6 * 510]; // actual is 3000
 	private int[] mTempPreFrame = new int[3 + 510];
+
+	private int mIndexRadarImg = 0;
 
 	public ProcessingData(String pathData) {
 		mPathData = pathData;
@@ -20,30 +23,49 @@ public class ProcessingData {
 		try {
 			InputStream in = new FileInputStream(mPathData);
 			byte[] frame = new byte[1024];
+			int firstPulseCode = -1;
 			while (in.read(frame) > 1) {
-				processingFrame(frame);
+				int[] frameInt = processingFrame(frame);
+				// Check condition create new radar image
+				if (firstPulseCode == -1) {
+					firstPulseCode = frameInt[2];
+				} else if (firstPulseCode - 12 <= frameInt[2] //
+						&& frameInt[2] <= firstPulseCode //
+						&& mIndexRadarImg > 3000) { // condition create new radar image
+					System.out.println(mIndexRadarImg);
+					saveRadarImg(mRadarImg);
+					mRadarImg = new int[20000][3000];
+					mIndexRadarImg = 0;
+					// in ảnh và reset mRadarImg
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void processingFrame(byte[] frame) {
+	private int[] processingFrame(byte[] frame) {
 		int[] frameInt = getFrameValue(frame);
 		if (frameInt[1] != mTempPreFrame[1]) { // compare PID
 			addToRadaImg(mTempPulseData);
-			mTempPulseData = new int[3000];
+			mTempPulseData = new int[6 * 510];
+			System.out.println(frameInt[2]);
 		}
+		mTempPreFrame = frameInt;
 		// push frame to pulse data
 		int FID = frameInt[0];
-		int PulseCode = frameInt[2];
 		for (int i = 3; i < frameInt.length; i++) {
 			mTempPulseData[FID * 510 + (i - 3)] = frameInt[i];
 		}
+		return frameInt;
 	}
 
 	private void addToRadaImg(int[] pulseData) {
-
+		for (int i = 0; i < 3000; i++) {
+			mRadarImg[mIndexRadarImg][i] = pulseData[i];
+		}
+		System.out.print(mIndexRadarImg + " : ");
+		mIndexRadarImg++;
 	}
 
 	private int[] getFrameValue(byte[] frame) {
@@ -57,6 +79,11 @@ public class ProcessingData {
 			index++;
 		}
 		return frameInt;
+	}
+
+	private void saveRadarImg(int[][] mRadarImg) {
+		// TODO Auto-generated method stub
+
 	}
 
 	private int getInt(byte b) {
